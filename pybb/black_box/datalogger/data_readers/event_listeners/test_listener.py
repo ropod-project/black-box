5b5f8b3766ed8af95cd8f8e463b7904514cbad5d
Dharmin <dharmingeo@gmail.com>
2019-09-02 20:13:25 +0200

    Updated config files and test listener

--- pybb/black_box/datalogger/data_readers/event_listeners/test_listener.py
+++ pybb/black_box/datalogger/data_readers/event_listeners/test_listener.py
@@ -9,23 +9,24 @@ class TestListener(EventListenerBase):
     """Test listener
     
     @name -- string (name of the listener)
-    @event_type -- string (['CHANGE'])
+    @event_type -- string (['ON_CHANGE', 'ON_STARTUP'])
     """
 
     def __init__(self, name, event_type, max_frequency, data_logger):
         super(TestListener, self).__init__(name, event_type, max_frequency, data_logger)
         self._last_logged_msg = None
+        self._logged = False
 
-    def run(self):
-        time.sleep(2)
-        while self.logging:
-            keys = rospy.get_param_names()
-            print("Num of rosparam", len(keys))
-            if self._last_logged_msg is None or \
-                    self._last_logged_msg + 1 < time.time():
-                self.data_logger.log_data(
-                        self.name, 
-                        time.time(), 
-                        {'time':time.time()})
-                self._last_logged_msg = time.time()
-            time.sleep(self.sleep_time)
+    def _get_data_on_change(self):
+        current_data = int(time.time())%10
+        if self._last_logged_msg is None or self._last_logged_msg != current_data:
+            self._last_logged_msg = current_data
+            return {'time':current_data}
+        return None
+    
+    def _get_data_on_startup(self):
+        if self._logged:
+            return None
+        current_data = int(time.time())%10
+        self._logged = True
+        return {'time':current_data}
