c84b747f103f786ceafae22961520424b051a143
Dharmin <dharmingeo@gmail.com>
2019-09-05 01:15:38 +0200

    Testing system info listener

---
+++ pybb/black_box/datalogger/data_readers/event_listeners/system_info_listener.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import time
+import rospy
+from std_msgs.msg import String, Empty
+from black_box.datalogger.data_readers.event_listeners.event_listener_base import EventListenerBase
+
+class SystemInfoListener(EventListenerBase):
+
+    """Listen for system info
+    """
+
+    def __init__(self, name, event_type, max_frequency, data_logger):
+        super(SystemInfoListener, self).__init__(name, event_type, max_frequency, data_logger)
+        self._logged = False
+        rospy.sleep(2)
+        print("before sub and pub")
+        self.ropod_get_info_pub = rospy.Publisher('/ropod_system_info/get_ropod_info',
+                                                  Empty,
+                                                  queue_size=1)
+        print("after pub")
+        self.ropod_info_sub = rospy.Subscriber('/ropod_system_info/ropod_info',
+                                               String,
+                                               self._get_ropod_info)
+        print("after sub")
+
+    def _get_data_on_startup(self):
+        if self._logged:
+            return None
+        current_data = int(time.time())%10
+        self._logged = True
+        return {'time':current_data}
+
+    def _get_ropod_info(self, msg):
+        print(msg)
